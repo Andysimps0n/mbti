2b862b3148714ee9c27b043c4a7bf9edf91e733f
Andy Kim <26eckim@wonhago.org>
2024-12-20 10:45:57 +0900

    font aded

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,10 +1,17 @@
 import "./globals.css";
 
+import { Gaegu } from 'next/font/google';
+
+const gaegu = Gaegu({
+  subsets: ['latin'], // Define the character set
+  weight: ['400', '700'], // Define the weights you need
+});
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={gaegu.className}>
         {children}
       </body>
     </html>
--- my-app/src/app/test/page.js
+++ my-app/src/app/test/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-        this is homes
+    <div className="main-cover border">
+      안녕하세요~
     </div>
   )
 }
