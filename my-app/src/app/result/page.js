a670d1ebe73acb2f745d4b1e7d61157af6c7c5a3
Andy Kim <26eckim@wonhago.org>
2024-12-20 20:34:03 +0900

    questions and UIs

---
+++ my-app/src/app/result/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      this is result
+    </div>
+  )
+}
+
+export default page
--- my-app/src/app/test/page.js
+++ my-app/src/app/test/page.js
@@ -1,9 +1,82 @@
+"use client"
+
 import React from 'react'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 function page() {
+
+  const router = useRouter()
+  const handleBottomClick = () => {
+    setcurrentQuestionNumber(currentQuestionNumber + 1)
+  }
+
+  const handleTopClick = () => {
+    setcurrentQuestionNumber(currentQuestionNumber + 1)
+  }
+  const [currentQuestionNumber, setcurrentQuestionNumber] = useState(0);
+  if (currentQuestionNumber == 9) {
+    router.push('/result')
+  }
+
+
+  const questions = [
+    {
+      question: "당신은 PT를 받는 사람입니다. \n \n 트레이너가 나의 식단을 물었을 때 나는",
+      option: ["들고있던 치킨을 숨긴다", "당당히 걸어간다"]
+    },
+    {
+      question: "피곤한 월요일 아침, 당신의 눈 앞에 빈백이 보입니다.",
+      option: ["묻지도 따지지도 않고 눕는다", "다음 수업을 준비하기 위해 지나친다"]
+    },
+    {
+      question: "학교에서 최저 시급 받으며 1주일 살기 (밥 무한 인터넷 무한)",
+      option: ["가능", "불가능"]
+    },
+    {
+      question: "복도를 지나가다가 친구가 괴롭힘 당하는 것을 봤다.",
+      option: ["선생님한테 알린다", "지나친다"]
+    },
+    {
+      question: "아무에게도 말하지 못하는 고민이 있을 때 나는",
+      option: ["가장 친한 친구에게 말한다", "혼자 해결한다"]
+    },
+    {
+      question: "나랑 가장 친한 친구는",
+      option: ["학교에 있다", "인터넷에 있다"]
+    },
+    {
+      question: "모둠 과제를 하면서 당신이 의견을 제시할 때 모둠원의 반응은",
+      option: ["별 말 아니겠지 하면서 무시한다", "자신의 말을 경청해준다"]
+    },
+    {
+      question: "발표에서 대본 숙지를 못해 점수를 잘 못 받은 당신, 자신에게 할 말은?",
+      option: ["너는 잘 하는게 없어", "다음 발표에서는 대본 숙지를 잘 해야겠다. 그래도 수고했어."]
+    },
+    {
+      question: "나는 내 삶의 목표와 방향을 분명히 이해하고 있나요?",
+      option: ["예", "아니오"]
+    },
+    {
+      question: "내 삶에서 지속적으로 성장하고 배우고 있다는 느낌이 드나요?",
+      option: ["예", "아니오"]
+    }
+  ];
+  
+
   return (
     <div className="main-cover border">
-      안녕하세요~
+      <div className="progress-bar">
+        <div className="progress-shower" style={{width : `${10 * currentQuestionNumber}%`}}></div>
+      </div>
+
+      <div className="question-container">{questions[currentQuestionNumber].question}</div>
+
+      <div className="button-container">
+        <div className="button" onClick={()=>{handleTopClick()}}>{questions[currentQuestionNumber].option[0]}</div>
+        <div className="button" onClick={()=>{handleBottomClick()}}>{questions[currentQuestionNumber].option[1]}</div>
+      </div>
+
     </div>
   )
 }
