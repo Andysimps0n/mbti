78b4774f9ba7b2031a8bf4f8e8ed2ae81335c009
Andy Kim <26eckim@wonhago.org>
2024-12-20 10:42:09 +0900

    init and home

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,5 +1,16 @@
+'use client'
+ 
+import { useRouter } from 'next/navigation'
+
 export default function Home() {
+
+  const router = useRouter()
+
   return (
-    <div></div>
+    <div className="main-cover border">
+
+      <div className="start-button" onClick={()=>{router.push('/test')}}></div>
+    </div>
+
   );
 }
